60dd1d7c9e16db92465eef4a5fc3d66dccc4bd74
Emma Strubell <emma.strubell@gmail.com>
2017-11-27 10:20:03 -0500

    ptb script

--- research/syntaxnet/ptb_parser_trainer.sh
+++ research/syntaxnet/ptb_parser_trainer.sh
@@ -1,27 +1,15 @@
 #!/bin/sh
-# Copyright 2016 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
 
-# A script to train the CONLL2017 baseline.
 set -e
 
-language=English
-output_dir=./trained-"$language"
+# PTB-specific defs
+name="ptb"
+data_dir="$DATA_DIR/wsj-parse-3.5.0"
+training_corpus="$data_dir/wsj02-21-trn.sdep.spos.conllu"
+dev_corpus="$data_dir/wsj22-dev.sdep.spos.conllu"
 
-training_corpus=$1
-dev_corpus=$2
+output_parent="trained"
+output_dir="$output_parent/$name"
 
 # bazel build -c opt //dragnn/tools:trainer //dragnn/conll2017:make_parser_spec
 
