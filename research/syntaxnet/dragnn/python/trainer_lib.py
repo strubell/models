b1a5d5929a6bf569a4e6ef39f74d76d178021273
Emma Strubell <emma.strubell@gmail.com>
2017-11-30 15:35:01 -0500

    debug

--- research/syntaxnet/dragnn/python/trainer_lib.py
+++ research/syntaxnet/dragnn/python/trainer_lib.py
@@ -87,7 +87,7 @@ def run_training_step(sess, trainer, train_corpus, batch_size):
   """Runs a single iteration of train_op on a randomly sampled batch."""
   batch = random.sample(train_corpus, batch_size)
   for b in batch:
-    print(text_format.Parse(b, sentence_pb2))
+    print(text_format.Parse(b, sentence_pb2._message))
   start_time = time.time()
   _, cost = sess.run([trainer['run'], trainer['cost']], feed_dict={trainer['input_batch']: batch})
   return cost, batch_size, time.time() - start_time
