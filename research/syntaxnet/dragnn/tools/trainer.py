25303550b09f2670b81d26dfc72e047900a14c62
Emma Strubell <emma.strubell@gmail.com>
2017-11-30 14:48:09 -0500

    try better hyperparams

--- research/syntaxnet/dragnn/tools/trainer.py
+++ research/syntaxnet/dragnn/tools/trainer.py
@@ -62,7 +62,7 @@ flags.DEFINE_string('resource_path', '', 'Path to constructed resources.')
 flags.DEFINE_string('hyperparams',
                     'momentum:0.9 '
                     'decay_steps:128000 dropout_rate:0.8 gradient_clip_norm:1 '
-                    'learning_method:"momentum" learning_rate:0.004 seed:1 '
+                    'learning_method:"momentum" learning_rate:0.04 seed:1 '
                     'use_moving_average:true',
                     'Hyperparameters of the model to train, either in ProtoBuf'
                     'text format or base64-encoded ProtoBuf text format.')
@@ -86,7 +86,7 @@ flags.DEFINE_integer('job_id', 0, 'The trainer will clear checkpoints if the '
                      'saved job id is less than the id this flag. If you want '
                      'training to start over, increment this id.')
 
-non_target_transition_systems = {'shift_only', 'once'}
+non_target_transition_systems = {'shift-only', 'once'}
 
 def main(unused_argv):
   logging.set_verbosity(logging.INFO)
